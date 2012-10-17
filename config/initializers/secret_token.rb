461187e7c0e23e60ef4c1d4a8c8f2d00980ab363
Chris Searle <chris@chrissearle.org>
2012-10-17 07:29:00 +0200

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homelan::Application.config.secret_token = '8adf31a149656a1192cf762c6487ab1da5773dd3fb0699106a59ab9707f10307f83582d75979822a19af2d74f45ea78a499b095fb8abf4611fde9f73e1f92f30'
